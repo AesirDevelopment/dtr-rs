7ea86652ea6ec3a8fd610123e2f95da5ea02f825
FerrielMelarpis <fmelarpis@freedom.tm>
2017-01-30 00:43:39 +0800

    Implemented echo server

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,14 @@
+extern crate iron;
+
+use iron::*;
+use std::io::Read;
+
 fn main() {
-    println!("Hello, world!");
+    Iron::new(|request: &mut Request| {
+        let mut buffer = String::new();
+        request.body.read_to_string(&mut buffer);
+        let response = buffer;
+
+        Ok(Response::with((status::Ok, response)))
+    }).http("localhost:3000").unwrap();
 }
